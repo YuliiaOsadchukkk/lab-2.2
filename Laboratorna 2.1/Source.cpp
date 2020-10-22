090bdbaa4664f8c9704b68041c0f6bef6bcec92a
YuliiaOsadchukkk <72582368+YuliiaOsadchukkk@users.noreply.github.com>
2020-10-22 04:18:12 +0300

    changed

--- Laboratorna 2.1/Source.cpp
+++ Laboratorna 2.1/Source.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	double n; // вхідний параметр
 	double m; // вхідний параметр
-	double z1; // результат обчислення першого виразу
-	//double z2; // результат обчислення другого виразу
+	//double z1; // результат обчислення першого виразу
+	double z2; // результат обчислення другого виразу
 
 	//ввід даних
 
@@ -17,13 +17,13 @@ int main()
 	cout << "m = "; cin >> m;
 
 	// обчислення
-	z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(m * m * m * n) + n * m + m * m - m);
-	//z2 = (sqrt(m) - sqrt(n)) / m;
+	//z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(m * m * m * n) + n * m + m * m - m);
+	z2 = (sqrt(m) - sqrt(n)) / m;
 
 	// виведення результатів
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
